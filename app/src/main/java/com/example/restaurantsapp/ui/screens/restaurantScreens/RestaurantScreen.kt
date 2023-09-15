18a13ec0c9e6c8f69a2a3623c15d677c4f94a0ca
Peter-cloud-web <petergichia165@gmail.com>
2023-09-15 09:20:13 +0300

    lift states from composables to viewmodel

--- app/src/main/java/com/example/restaurantsapp/ui/screens/restaurantScreens/RestaurantScreen.kt
+++ app/src/main/java/com/example/restaurantsapp/ui/screens/restaurantScreens/RestaurantScreen.kt
@@ -5,11 +5,8 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.unit.dp
-import com.example.restaurantsapp.data.model.Restaurant
 import com.example.restaurantsapp.data.model.RestaurantResponse
 import com.example.restaurantsapp.ui.screens.restaurantComponents.RestaurantItem
 import com.example.restaurantsapp.viewModel.RestaurantViewModel
@@ -21,33 +18,16 @@ fun RestaurantScreen(restaurantResponse: RestaurantResponse) {
     val viewModel = remember {
         RestaurantViewModel(restaurantResponse)
     }
-
-    val state: MutableState<List<Restaurant>> =
-        remember {
-            mutableStateOf(viewModel.getRestaurants().restaurants)
-        }
-
     LazyColumn(
         contentPadding = PaddingValues(
             vertical = 8.dp,
             horizontal = 8.dp
         )
     ) {
-
-        items(state.value) { restaurant ->
+        items(viewModel.state.value) { restaurant ->
             RestaurantItem(item = restaurant) { id ->
-                val restaurants = state.value.toMutableList()
-                val itemIndex = restaurants.indexOfFirst {
-                    it.id == id
-                }
-
-                val item = restaurants[itemIndex]
-
-                restaurants[itemIndex] = item.copy(isFavourite = !item.isFavourite)
-                state.value = restaurants
+                viewModel.toggleFavourite(id)
             }
-
-
         }
     }
 }
--- app/src/main/java/com/example/restaurantsapp/viewModel/RestaurantViewModel.kt
+++ app/src/main/java/com/example/restaurantsapp/viewModel/RestaurantViewModel.kt
@@ -1,9 +1,21 @@
 package com.example.restaurantsapp.viewModel
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.restaurantsapp.data.model.RestaurantResponse
 
 class RestaurantViewModel(private val restaurantResponse: RestaurantResponse) : ViewModel() {
+
+    val state = mutableStateOf(restaurantResponse.restaurants)
+
+    fun toggleFavourite(id: Int) {
+        val restaurants = state.value.toMutableList()
+        val itemIndex = restaurants.indexOfFirst { it.id == id }
+        val item = restaurants[itemIndex]
+        restaurants[itemIndex] = item.copy(isFavourite = !item.isFavourite)
+        state.value = restaurants
+
+    }
 
     fun getRestaurants(): RestaurantResponse {
         return restaurantResponse
