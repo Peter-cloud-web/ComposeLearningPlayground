72a4307e3b4058e14bd366eb2cc169cf2c5452c3
Peter-cloud-web <petergichia165@gmail.com>
2023-09-19 09:31:19 +0300

    order restaurant by Id

--- app/src/main/java/com/example/restaurantsapp/api/RestaurantApiService.kt
+++ app/src/main/java/com/example/restaurantsapp/api/RestaurantApiService.kt
@@ -3,9 +3,15 @@ package com.example.restaurantsapp.api
 import com.example.restaurantsapp.data.model.Restaurant
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface RestaurantApiService {
 
     @GET("restaurants.json")
     suspend fun getRestaurants(): List<Restaurant>
+
+    @GET("restaurants.json?orderBy=\"r_id\"")
+    suspend fun getRestaurant(
+        @Query("equalTo") id:Int):Unit
+
 }
